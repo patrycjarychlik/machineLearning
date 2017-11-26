f4346618548666351332670f436eefa0fb071087
ZWUS\zwsfke <filip.klose@rail.bombardier.com>
2017-11-26 18:36:44 +0100

    Add user detail dao

---
+++ app/src/main/java/com/patrycja/filip/machinelearning/db/dao/UserDetailDao.java
@@ -0,0 +1,20 @@
+package com.patrycja.filip.machinelearning.db.dao;
+
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import android.arch.persistence.room.Query;
+
+import com.patrycja.filip.machinelearning.db.entity.UserDetailEntity;
+
+/**
+ * Created by zwsfke on 2017-11-26.
+ */
+@Dao
+public interface UserDetailDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    void insert(UserDetailEntity userDetail);
+
+    @Query("SELECT * FROM user_details WHERE id = :userDetailId")
+    UserDetailEntity loadById(int userDetailId);
+}
