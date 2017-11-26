7985e00fc25f4dac12b55604850ea577e720abf2
phillipklose <filip.klose@gmail.com>
2017-11-26 17:41:31 +0100

    Add user detail entity

---
+++ app/src/main/java/com/patrycja/filip/machinelearning/db/entity/UserDetailEntity.java
@@ -0,0 +1,20 @@
+package com.patrycja.filip.machinelearning.db.entity;
+
+import android.arch.persistence.room.ColumnInfo;
+import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.PrimaryKey;
+
+import lombok.Data;
+
+/**
+ * Created by zwsfke on 2017-11-26.
+ */
+@Data
+@Entity(tableName = "user_details")
+public class UserDetailEntity {
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+
+    @ColumnInfo(name = "total_exp")
+    private int totalExp;
+}
