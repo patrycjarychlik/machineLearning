c5aa274ebb31da53df30e01180de11b41c135008
phillipklose <filip.klose@gmail.com>
2017-11-26 07:57:16 +0100

    Remove unused imports

--- app/src/main/java/com/patrycja/filip/machinelearning/HomeActivity.java
+++ app/src/main/java/com/patrycja/filip/machinelearning/HomeActivity.java
@@ -1,22 +1,12 @@
 package com.patrycja.filip.machinelearning;
 
-import android.graphics.drawable.AnimatedVectorDrawable;
-import android.graphics.drawable.Drawable;
-import android.graphics.drawable.DrawableWrapper;
-import android.graphics.drawable.RotateDrawable;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v4.widget.CircularProgressDrawable;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.ProgressBar;
 
