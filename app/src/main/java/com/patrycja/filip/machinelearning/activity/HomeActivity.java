0a6e5f936dfb5a908b33ef017ecf44c9f5e152be
ZWUS\zwsfke <filip.klose@rail.bombardier.com>
2017-11-27 18:24:21 +0100

    Add export database functionality to home activity

--- app/src/main/java/com/patrycja/filip/machinelearning/activity/HomeActivity.java
+++ app/src/main/java/com/patrycja/filip/machinelearning/activity/HomeActivity.java
@@ -1,10 +1,14 @@
 package com.patrycja.filip.machinelearning.activity;
 
+import android.Manifest;
 import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.design.widget.NavigationView;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
@@ -12,9 +16,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.patrycja.filip.machinelearning.R;
 import com.patrycja.filip.machinelearning.persistence.db.entity.ChapterEntity;
+import com.patrycja.filip.machinelearning.persistence.db.exception.DbExportException;
+import com.patrycja.filip.machinelearning.persistence.db.helper.AppDatabaseBackupHelper;
 import com.patrycja.filip.machinelearning.persistence.viewmodel.ChapterViewModel;
 
 import java.util.List;
@@ -47,6 +54,28 @@ public class HomeActivity extends AppCompatActivity
         });
     }
 
+    private void exportDatabaseToDownloadsDir() {
+        if (isReadWritePermissionsGranted() && needRequestForPermissions()) {
+            requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE, Manifest.permission.READ_EXTERNAL_STORAGE}, 1);
+        }
+        try {
+            AppDatabaseBackupHelper.exportDB();
+            Toast.makeText(this, "DB Backup successfully created!", Toast.LENGTH_SHORT).show();
+        } catch (DbExportException e) {
+            Log.println(Log.ERROR, "DbExportError", e.getMessage());
+            Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private boolean needRequestForPermissions() {
+        return Build.VERSION.SDK_INT > 22;
+    }
+
+    private boolean isReadWritePermissionsGranted() {
+        return ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                && ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED;
+    }
+
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -74,6 +103,8 @@ public class HomeActivity extends AppCompatActivity
 
         } else if (id == R.id.nav_invite_friends) {
 
+        } else if (id == R.id.nav_export_db) {
+            exportDatabaseToDownloadsDir();
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
