cc39b62edaec4ac8f9f426b84c083206429ad822
ZWUS\zwsfke <filip.klose@rail.bombardier.com>
2017-12-09 15:00:33 +0100

    Add firs time launch to shared prefs

--- app/src/main/java/com/patrycja/filip/machinelearning/commons/AppSharedPreferences.java
+++ app/src/main/java/com/patrycja/filip/machinelearning/commons/AppSharedPreferences.java
@@ -10,15 +10,17 @@ public class AppSharedPreferences {
     private static AppSharedPreferences instance;
     private SharedPreferences sharedPreferences;
 
+    private static final int SHARED_PRIVATE_MODE = 0;
     private static final String APP_PREFERENCES = "app_preferences";
     private static final String LAUNCH_COUNT = "launch_count";
     private static final String IS_APP_RATED = "is_app_rated";
+    private static final String IS_FIRST_TIME_LAUNCHED = "is_first_time_launched";
 
     private AppSharedPreferences() {
     }
 
     private AppSharedPreferences(Context context) {
-        this.sharedPreferences = context.getSharedPreferences(APP_PREFERENCES, 0);
+        sharedPreferences = context.getSharedPreferences(APP_PREFERENCES, SHARED_PRIVATE_MODE);
     }
 
     public static AppSharedPreferences getInstance(Context context) {
@@ -29,29 +31,39 @@ public class AppSharedPreferences {
     }
 
     public int getLaunchCount() {
-        return this.sharedPreferences.getInt(LAUNCH_COUNT, 0);
+        return sharedPreferences.getInt(LAUNCH_COUNT, SHARED_PRIVATE_MODE);
     }
 
     public void incrementLaunchCount() {
         int launchCount = getLaunchCount();
-        SharedPreferences.Editor editor = this.sharedPreferences.edit();
+        SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putInt(LAUNCH_COUNT, ++launchCount);
         editor.commit();
     }
 
     public void resetLaunchCount() {
-        SharedPreferences.Editor editor = this.sharedPreferences.edit();
+        SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.remove(LAUNCH_COUNT);
         editor.commit();
     }
 
     public boolean isAppRated() {
-        return this.sharedPreferences.getBoolean(IS_APP_RATED, false);
+        return sharedPreferences.getBoolean(IS_APP_RATED, false);
     }
 
     public void setIsAppRated(boolean isAppRated) {
-        SharedPreferences.Editor editor = this.sharedPreferences.edit();
+        SharedPreferences.Editor editor = sharedPreferences.edit();
         editor.putBoolean(IS_APP_RATED, isAppRated);
+        editor.commit();
+    }
+
+    public boolean isFirstTimeLaunched() {
+        return sharedPreferences.getBoolean(IS_FIRST_TIME_LAUNCHED, false);
+    }
+
+    public void setIsFirstTimeLaunched() {
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putBoolean(IS_FIRST_TIME_LAUNCHED, true);
         editor.commit();
     }
 }
