327ab36f11a3010b3a6833a221d48debcbe90702
ZWUS\zwsfke <filip.klose@rail.bombardier.com>
2018-02-13 23:58:24 +0100

    Remove sound button from home fragment

--- app/src/main/java/com/patrycja/filip/machinelearning/fragment/HomeFragment.java
+++ app/src/main/java/com/patrycja/filip/machinelearning/fragment/HomeFragment.java
@@ -6,18 +6,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.GridView;
-import android.widget.TextView;
 
 import com.patrycja.filip.machinelearning.R;
 import com.patrycja.filip.machinelearning.activity.ChapterActivity;
 import com.patrycja.filip.machinelearning.adapter.chapters.ChapterAdapter;
-import com.patrycja.filip.machinelearning.commons.SoundPlayer;
 import com.patrycja.filip.machinelearning.persistence.db.entity.ChapterEntity;
 import com.patrycja.filip.machinelearning.persistence.viewmodel.ChapterViewModel;
 
@@ -33,23 +29,15 @@ public class HomeFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_home, container, false);
     }
 
-
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getActivity().setTitle("Home");
 
-        // TODO temporary sound test button
-        Button btnCorrectSound = (Button) view.findViewById(R.id.btn_correct_sound);
-        btnCorrectSound.setOnClickListener(btnClick -> SoundPlayer.playCorrectAnswerSound(this.getActivity()));
-
-
         final ChapterViewModel chapterViewModel = ViewModelProviders.of(this).get(ChapterViewModel.class);
         chapterViewModel.getObservableChapters().observe(this, new Observer<List<ChapterEntity>>() {
             @Override
             public void onChanged(@Nullable List<ChapterEntity> chapterEntities) {
-                logModelContent(chapterEntities);
-
                 ChapterAdapter adapter = new ChapterAdapter(getContext(), chapterEntities, view);
                 GridView gridView = (GridView) view.findViewById(R.id.gridview);
                 gridView.setAdapter(adapter);
@@ -64,13 +52,6 @@ public class HomeFragment extends Fragment {
                 });
             }
         });
-    }
-
-    private void logModelContent(@Nullable List<ChapterEntity> chapterEntities) {
-        Log.println(Log.INFO, "TEST_VIEW_MODEL", "List of chapters: ");
-        for (ChapterEntity chapter : chapterEntities) {
-            Log.println(Log.INFO, "TEST_VIEW_MODEL", "chapter: " + chapter.getTitle() + "-" + chapter.getDescription());
-        }
     }
 
 }
--- app/src/main/java/com/patrycja/filip/machinelearning/fragment/SettingsFragment.java
+++ app/src/main/java/com/patrycja/filip/machinelearning/fragment/SettingsFragment.java
@@ -6,8 +6,10 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.patrycja.filip.machinelearning.R;
+import com.patrycja.filip.machinelearning.commons.SoundPlayer;
 
 /**
  * Created by zwsfke on 2017-11-27.
@@ -24,6 +26,10 @@ public class SettingsFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         getActivity().setTitle("Settings");
+
+        //Todo: Use this sound when correct answer is given in quiz
+        Button btnCorrectSound = (Button) view.findViewById(R.id.btn_correct_sound);
+        btnCorrectSound.setOnClickListener(btnClick -> SoundPlayer.playCorrectAnswerSound(this.getActivity()));
     }
 
 }
